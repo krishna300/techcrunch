31164e367d2fe422a5a9b7b5a0cdf16ba8be87b1
Krishna Kondapalli <srikrishnakvs472@gmail.com>
2020-06-01 22:31:09 +0530

    Updates

---
+++ precache-manifest.d7e9cc98f909a8e01500769f9fa2db4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab064411dcffa108c8cbc9587126fff",
+    "url": "/techcrunch/index.html"
+  },
+  {
+    "revision": "097bc0f268f6291811ef",
+    "url": "/techcrunch/static/css/main.293214ee.chunk.css"
+  },
+  {
+    "revision": "4f05b031dc52b2a6e450",
+    "url": "/techcrunch/static/js/2.d4c75dbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techcrunch/static/js/2.d4c75dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097bc0f268f6291811ef",
+    "url": "/techcrunch/static/js/main.366c29c8.chunk.js"
+  },
+  {
+    "revision": "ac1dcbe295dfa0453d6e",
+    "url": "/techcrunch/static/js/runtime-main.e7f9341e.js"
+  }
+]);
